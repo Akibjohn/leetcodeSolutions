76425dea106f499bddb66b9a63d4dd8bc93a46ee
Mohd Akib <109415678+Akibjohn@users.noreply.github.com>
2022-09-08 20:01:25 +0530

    Time: 13 ms (14.78%), Space: 42.6 MB (23.18%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,30 +1,35 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        // HashMap<Character ,Integer> hms=new HashMap<>();
-        // HashMap<Character ,Integer> hmt=new HashMap<>();
-        // for(char c:s.toCharArray()){
-        //     hms.put(c,hms.getOrDefault(c,0)+1);
-        // }
-        // for(char c:t.toCharArray()){
-        //     hmt.put(c,hmt.getOrDefault(c,0)+1);
-        // }
-        // for(char key:hmt.keySet()){
-        //     if(!hms.containsKey(key)){
-        //         return key;
-        //     }
-        // }
-        // return '0';
-        
-        int ans=0;
-        
-        for(Character c:t.toCharArray()){
-            ans+=(int)c;
+        HashMap<Character ,Integer> hms=new HashMap<>();
+        HashMap<Character ,Integer> hmt=new HashMap<>();
+        for(char c:s.toCharArray()){
+            hms.put(c,hms.getOrDefault(c,0)+1);
         }
-        for(Character c:s.toCharArray()){
-            ans-=(int)c;
+        for(char c:t.toCharArray()){
+            hmt.put(c,hmt.getOrDefault(c,0)+1);
         }
+        for(char key:hmt.keySet()){
+            if(!hms.containsKey(key)){
+                return key;
+            }
+            else{
+                if(hmt.get(key) > hms.get(key)){
+                    return key;
+                }
+            }
+        }
+        return '0';
         
-        return (char)ans;
+//         int ans=0;
+        
+//         for(Character c:t.toCharArray()){
+//             ans+=(int)c;
+//         }
+//         for(Character c:s.toCharArray()){
+//             ans-=(int)c;
+//         }
+        
+//         return (char)ans;
         
         
     }
