193ea63cb897f330d6bfa372980505eccd481c93
Mohd Akib <109415678+Akibjohn@users.noreply.github.com>
2022-09-08 17:15:03 +0530

    Time: 4 ms (40.16%), Space: 41.9 MB (77.75%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.java
@@ -0,0 +1,31 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        // HashMap<Character ,Integer> hms=new HashMap<>();
+        // HashMap<Character ,Integer> hmt=new HashMap<>();
+        // for(char c:s.toCharArray()){
+        //     hms.put(c,hms.getOrDefault(c,0)+1);
+        // }
+        // for(char c:t.toCharArray()){
+        //     hmt.put(c,hmt.getOrDefault(c,0)+1);
+        // }
+        // for(char key:hmt.keySet()){
+        //     if(!hms.containsKey(key)){
+        //         return key;
+        //     }
+        // }
+        // return '0';
+        
+        int ans=0;
+        
+        for(Character c:t.toCharArray()){
+            ans+=(int)c;
+        }
+        for(Character c:s.toCharArray()){
+            ans-=(int)c;
+        }
+        
+        return (char)ans;
+        
+        
+    }
+}
