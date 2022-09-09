48112b6fd00af7194333f48efb1021af015ade25
Mohd Akib <109415678+Akibjohn@users.noreply.github.com>
2022-09-09 16:23:55 +0530

    Time: 1 ms (20.20%), Space: 40.2 MB (82.65%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        long start=2;
+        long end=num/2;
+        while(start<=end){
+            long mid=start+(end-start)/2;
+            if(mid*mid==num){
+                return true;
+            }
+            else if(mid*mid<num){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return false;
+    }
+}
