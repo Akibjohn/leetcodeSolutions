8fa7a4c15b942835a923a8d9c1e2c43bd514b7b9
Mohd Akib <109415678+Akibjohn@users.noreply.github.com>
2022-09-14 15:10:29 +0530

    Time: 9 ms (7.39%), Space: 40.9 MB (86.29%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,9 +1,32 @@
 class Solution {
+    public int find(int[] arr){
+        int si=0;
+        int ei=arr.length-1;
+        int ans=-1;
+        while(si<=ei){
+            int mid=si+(ei-si)/2;
+            if(arr[mid]==0){
+                si=mid+1;
+            }else{
+                ans=mid;
+                ei=mid-1;
+            }
+        }return ans;
+    }
     public int minimumOperations(int[] nums) {
-        HashSet<Integer> hs=new HashSet<>();
-        for(int i: nums){
-            if(i!=0) hs.add(i);
+        Arrays.sort(nums);
+        int count=0;
+        while(nums[nums.length-1]!=0){
+            count++;
+            int idx=find(nums);
+            int value=nums[idx];
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]!=0){
+                    nums[i]-=value;
+                }
+            }
         }
-        return hs.size();
+        return count;
+        
     }
 }
