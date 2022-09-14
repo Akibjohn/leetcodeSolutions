a0e38fe6a118be55bdd46872bd4902df1bbb5012
Mohd Akib <109415678+Akibjohn@users.noreply.github.com>
2022-09-14 15:33:12 +0530

    Time: 22 ms (11.83%), Space: 54.9 MB (73.26%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        Stack<int[]> st = new Stack<>();
-        //st = [];
-        for(int[] oned : intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        Stack<int[]> st=new Stack<>();
+        for(int[] oned: intervals){
             if(st.isEmpty()) st.push(oned);
-            // st = [{1,3}]
-            else {
-                if(oned[0] <= st.peek()[1]){ // st.peek()[1] = [1,3] {
-                    st.peek()[1] = Math.max(st.peek()[1],oned[1]);
-                } else {
+            else{
+                if(oned[0]<=st.peek()[1]){
+                    int[] poped=st.pop();
+                    st.push(new int[]{poped[0],Math.max(poped[1],oned[1])});
+                }else {
                     st.push(oned);
                 }
             }
         }
-        int ans[][] = new int[st.size()][2], idx = 0;
-        for(int[] oned : st) ans[idx ++] = oned;
+        int ans[][]=new int[st.size()][2];
+        int idx=0;
+        for(int[] oned : st){
+            ans[idx++]=oned;
+        }
         return ans;
+        
     }
 }
