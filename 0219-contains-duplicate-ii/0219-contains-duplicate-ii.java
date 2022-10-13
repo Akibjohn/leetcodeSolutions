e2036fdd612513df6bf6192895d9df67a474fe14
Mohd Akib <109415678+Akibjohn@users.noreply.github.com>
2022-10-13 16:39:28 +0530

    Time: 59 ms (30.18%), Space: 94.1 MB (38.15%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,28 +1,12 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int val;
-        int idx;
-        Pair(int val, int idx){
-            this.val=val;
-            this.idx=idx;
-            
-        }
-        public int compareTo(Pair other){
-            return this.val - other.val;
-        }
-    }
-    public boolean containsNearbyDuplicate(int[] nums, int target) {
-        Pair[] p=new Pair[nums.length];
-        for(int i=0;i<nums.length;i++) p[i]=new Pair(nums[i],i);
-        
-        Arrays.sort(p);
-        
-        for(int j=1;j<nums.length;j++){
-            int i=j-1;
-            int k=j;
-            if(p[i].val==p[k].val && p[k].idx-p[i].idx<=target) return true;
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                if(i-hm.get(nums[i])<=k) return true;
+            }
+            hm.put(nums[i],i);
         }
         return false;
-        
     }
 }
